983e99424db7d0b7fa6fdd4a6e8d6613a83540dc
Niyarkalita2021 <82107686+Niyarkalita2021@users.noreply.github.com>
2021-07-08 21:19:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,9 @@ var Content = event.results[0][0].transcript;
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-
     if(Content =="Take my selfie")
     {
-        console.log("taking selfie in 5 seconds");
+        console.log("taking selfie in 5 seconds");   
         speak();
     }
 }
@@ -34,12 +33,33 @@ function speak(){
 
     Webcam.attach(camera);
 
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
+
 }
 
 camera = document.getElementById("camera");
 Webcam.set({
         width: 360,
         height:260,
-        image_format : 'png',
-        png_quality: 90
+        image_format : 'jpeg',
+        jpeg_quality: 90
 });
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+          document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
